9ca8c51e4ed405833daa2c637a8939a21807217a
george schaefer <georgeschaef@gmail.com>
2021-07-02 11:35:27 -0500

    built app

---
+++ web/build/precache-manifest.0a2d6a94288b169870815a2b1c3f73c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa112b08bd1f546b404235e30f978747",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc3ac2d2ba045e1f61f",
+    "url": "/static/css/main.72f14ccc.chunk.css"
+  },
+  {
+    "revision": "dce56f58dc6041a35add",
+    "url": "/static/js/2.ff3a2396.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.ff3a2396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc3ac2d2ba045e1f61f",
+    "url": "/static/js/main.5255ac3c.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
